2d7c6d1eb1bf85978c339594c16b82f2692c56c1
Tyler McAllister <mcallistertyler95@gmail.com>
2021-08-21 11:14:59 +0200

    added argument for shuffling or not

--- src/com/cardgame/blackjack/Blackjack.java
+++ src/com/cardgame/blackjack/Blackjack.java
@@ -8,14 +8,21 @@ import static com.cardgame.blackjack.DeckFileReader.readDeckFile;
 final class Blackjack {
     public static void main(String[] args) {
         String filePath = "";
+        boolean shuffleCheck = true;
         if (args.length > 0) {
            filePath = args[0];
+        }
+        if (args.length == 2) {
+            if (args[1].equalsIgnoreCase("false")) {
+                shuffleCheck = false;
+                System.out.println("Shuffling turned off. Using deck as provided.");
+            }
         }
         List<String> deckFile = readDeckFile(filePath);
         Deck deck = new Deck(deckFile);
         Player sam = new Player("sam");
         Player dealer = new Player("dealer");
-        GameState blackjack = new GameState(sam, dealer, deck, true);
+        GameState blackjack = new GameState(sam, dealer, deck, shuffleCheck);
         blackjack.startGame();
     }
 
